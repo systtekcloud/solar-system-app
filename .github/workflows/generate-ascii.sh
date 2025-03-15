70b0dced8e824f6a25be954602b400f0c865c33e
Sergi P. Guillem <sergi@systtekcloud.com>
2025-03-15 16:59:52 +0100

    Added a scripts to execute cowsay

--- .github/workflows/generate-ascii.sh
+++ .github/workflows/generate-ascii.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 sudo apt-get install cowsay -y
 cowsay -f lion "Run for cover, I am a LION....RAWR" >> lion.txt
-grep -i "dragon" dragon.txt
+grep -i "lion" lion.txt
 cat lion.txt
 ls -ltra
