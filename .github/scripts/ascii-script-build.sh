c236ed0d867bb828f29162115766dfd9afd4e61c
Sergi P. Guillem <sergi@systtekcloud.com>
2025-03-15 16:34:35 +0100

    Added a scripts to execute cowsay

---
+++ .github/scripts/ascii-script-build.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> ./dragon.txt
---
+++ .github/scripts/ascii-script-deploy.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+cat dragon.txt
+echo Deploying ... .. .
